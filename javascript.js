06179c8159f35268e9e9dc80394122712bdfa46a
joao969 <joao_marcos3@hotmail.com>
2024-03-22 20:59:21 -0300

    att3

---
+++ javascript.js
@@ -0,0 +1,26 @@
+const sr= ScrollReveal({
+    distance :'10%',
+    duration: 2000,
+    delay: 5,
+    reset:true
+  });
+  
+  sr.reveal('h1',{delay:100, origin:'top'});
+  sr.reveal('h2',{delay:100, origin:'top'});
+  sr.reveal('h3',{delay:100, origin:'top'});
+  sr.reveal('h4',{delay:-100, origin:'top'});
+  sr.reveal('.img1',{delay:100, origin:'right'});
+  sr.reveal('.img3',{delay:100, origin:'right'});
+  sr.reveal('.img4',{delay:100, origin:'right'});
+  sr.reveal('.logo',{delay:100, origin:'right'});
+  sr.reveal('.trab1',{delay:-100, origin:'left'});
+  sr.reveal('.trab2',{delay:-100, origin:'right'});
+  sr.reveal('.trab3',{delay:-100, origin:'left'});
+ /* ------- FUNCAO ANCORA --------*/
+var $doc = $('html, body');
+$('.').click(function() {
+  $doc.animate({
+      scrollTop: $( $.attr(this,'href')).offset().top
+  },500);
+  return false;
+});
