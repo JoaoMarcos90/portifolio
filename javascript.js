7a0ccb5c5183f529cf66be701c029001d032814c
joao969 <joao_marcos3@hotmail.com>
2024-03-28 22:13:48 -0300

    att8

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 const sr= ScrollReveal({
-    distance :'20%',
+    distance :'30%',
     duration: 2000,
     delay: 5,
     reset:true
@@ -10,11 +10,12 @@ const sr= ScrollReveal({
   sr.reveal('h3',{delay:100, origin:'top'});
   sr.reveal('h4',{delay:-100, origin:'top'});
   sr.reveal('.linha1',{delay:100, origin:'top'});
+  sr.reveal('.linha2',{delay:100, origin:'left'});
   sr.reveal('.text1',{delay:-200, origin:'left'});
   sr.reveal('.text2',{delay:-100, origin:'top'});
-  sr.reveal('.icone1',{delay:-100, origin:'top'});
-  sr.reveal('.icone2',{delay:-100, origin:'top'});
-  sr.reveal('.icone3',{delay:-100, origin:'top'});
+  sr.reveal('.icone1',{delay:200, origin:'top'});
+  sr.reveal('.icone2',{delay:300, origin:'top'});
+  sr.reveal('.icone3',{delay:500, origin:'top'});
  /* ------- FUNCAO ANCORA --------*/
 var $doc = $('html, body');
 $('.').click(function() {
