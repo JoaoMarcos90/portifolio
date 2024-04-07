49274fb19bc4f98332ad796c1dada75d8d716735
joao969 <joao_marcos3@hotmail.com>
2024-04-06 22:23:35 -0300

    att10

--- javascript.js
+++ javascript.js
@@ -9,14 +9,15 @@ const sr= ScrollReveal({
   sr.reveal('h2',{delay:100, origin:'top'});
   sr.reveal('h3',{delay:100, origin:'top'});
   sr.reveal('h4',{delay:-100, origin:'top'});
-  sr.reveal('.linha1',{delay:100, origin:'top'});
+  sr.reveal('.linha1',{delay:-400, origin:'left'});
   sr.reveal('.linha2',{delay:100, origin:'left'});
   sr.reveal('.linha3',{delay:100, origin:'right'});
-  sr.reveal('.text1',{delay:-200, origin:'left'});
-  sr.reveal('.text2',{delay:-100, origin:'top'});
-  sr.reveal('.icone1',{delay:200, origin:'top'});
-  sr.reveal('.icone2',{delay:300, origin:'top'});
-  sr.reveal('.icone3',{delay:500, origin:'top'});
+  sr.reveal('.text1',{delay:-800, origin:'left'});
+  sr.reveal('.text2',{delay:-700, origin:'left'});
+  sr.reveal('.icone1',{delay:-200, origin:'top'});
+  sr.reveal('.icone2',{delay:-300, origin:'top'});
+  sr.reveal('.icone3',{delay:-500, origin:'top'});
+  sr.reveal('.imgsomos',{delay:-400, origin:'left'});
  /* ------- FUNCAO ANCORA --------*/
 var $doc = $('html, body');
 $('.').click(function() {
