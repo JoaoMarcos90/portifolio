376cfa0c40549f6f9aa8b0b5e95bdf0fca7d9c5d
joao969 <joao_marcos3@hotmail.com>
2024-04-15 20:58:27 -0300

    att11

--- javascript.js
+++ javascript.js
@@ -5,15 +5,13 @@ const sr= ScrollReveal({
     reset:true
   });
   
-  sr.reveal('h1',{delay:100, origin:'top'});
-  sr.reveal('h2',{delay:100, origin:'top'});
-  sr.reveal('h3',{delay:100, origin:'top'});
+  sr.reveal('h1',{delay:-700, origin:'top'});
+  sr.reveal('h2',{delay:-700, origin:'top'});
+  sr.reveal('h3',{delay:-700, origin:'top'});
   sr.reveal('h4',{delay:-100, origin:'top'});
-  sr.reveal('.linha1',{delay:-400, origin:'left'});
-  sr.reveal('.linha2',{delay:100, origin:'left'});
-  sr.reveal('.linha3',{delay:100, origin:'right'});
-  sr.reveal('.text1',{delay:-800, origin:'left'});
-  sr.reveal('.text2',{delay:-700, origin:'left'});
+  sr.reveal('.linha2',{delay:-800, origin:'left'});
+  sr.reveal('.linha3',{delay:-800, origin:'right'});
+  sr.reveal('.text2',{delay:-600, origin:'left'});
   sr.reveal('.icone1',{delay:-200, origin:'top'});
   sr.reveal('.icone2',{delay:-300, origin:'top'});
   sr.reveal('.icone3',{delay:-500, origin:'top'});
